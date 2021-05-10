265888653e902aa89df26e81b5fd43781ab2fab3
Kevin Hare <kchare8@gmail.com>
2021-05-10 00:16:38 -0400

    Clean up on aisle 5: removing extraneous comments, info, etc.

--- stock_predict/models/horovod_mpiuser_nodes.sh
+++ stock_predict/models/horovod_mpiuser_nodes.sh
@@ -1,6 +1,5 @@
 # Install pip
 sudo apt install -y python3-pip
-#pip3 install --upgrade pip
 python3.8 -m pip install --upgrade pip
 # ^ Stuff on pip needs to be installed like this instead for Python3.8 to find them
 
@@ -10,8 +9,6 @@ pip3.8 install --upgrade tensorflow
 python3.8 -c "import tensorflow as tf;print(tf.reduce_sum(tf.random.normal([1000, 1000])))"
 
 # Install horovod
-#HOROVOD_WITH_TENSORFLOW=1 HOROVOD_GPU_OPERATIONS=NCCL HOROVOD_WITH_MPI=1 python3.8 -m pip install --no-cache-dir horovod[tensorflow,keras]
-#export PATH="/home/ubuntu/.local/bin:$PATH" # This is only for the current session
 HOROVOD_WITH_TENSORFLOW=1 HOROVOD_GPU_OPERATIONS=NCCL HOROVOD_WITH_MPI=1 pip3.8 install --no-cache-dir horovod[tensorflow,keras]
 export PATH="/home/ubuntu/.local/bin:$PATH"
 export PATH="/home/mpiuser/.local/bin:$PATH"
--- stock_predict/predict/stock_client.py
+++ stock_predict/predict/stock_client.py
@@ -1,10 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed May  5 13:32:25 2021
-
-@author: junkaiong
-"""
 ## referenced Harvard CS205 Course Material Lecture C3
 ## https://harvard-iacs.github.io/2021-CS205/lectures/C3/ 
 
